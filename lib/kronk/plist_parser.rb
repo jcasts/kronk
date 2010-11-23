e8497ac1905e7af1ef7afaa38ed2c370c6b101ff
jcastagna <jeremie.castagna@gmail.com>
2010-11-23 11:19:52 -0800

    fixed plist parser

--- lib/kronk/plist_parser.rb
+++ lib/kronk/plist_parser.rb
@@ -3,13 +3,13 @@ class Kronk
   ##
   # Simple plist wrapper to add :parse method.
 
-  class PlistParser < Plist
+  class PlistParser
 
     ##
     # Alias for Plist.parse_xml
 
     def self.parse plist
-      parse_xml plist
+      Plist.parse_xml plist
     end
   end
 end
