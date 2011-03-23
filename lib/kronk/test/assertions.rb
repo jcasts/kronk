896eeeb962662e33b2a132b40e829c2cd2b60704
jcastagna <jeremie.castagna@gmail.com>
2011-03-23 12:53:03 -0700

    fixed and added tests for custom assertions

--- lib/kronk/test/assertions.rb
+++ lib/kronk/test/assertions.rb
@@ -23,19 +23,37 @@ class Kronk
 
 
       ##
+      # Assert that the given path doesn't exist in data.
+      # Supports all DataSet#find_data path types.
+
+      def assert_no_data_at data, path, msg=nil
+        msg ||= "Data found at #{path.inspect} for #{data.inspect}"
+        found = false
+
+        data_set = Kronk::DataSet.new data
+        data_set.find_data path do |d,k,p|
+          found = true
+          break
+        end
+
+        assert !found, msg
+      end
+
+
+      ##
       # Assert that at least one data point found with the given path is equal
       # to the given match.
       # Supports all DataSet#find_data path types.
 
-      def assert_data_at_equals data, path, match, msg=nil
+      def assert_data_at_equal data, path, match, msg=nil
         last_data = nil
         found     = false
 
         data_set = Kronk::DataSet.new data
         data_set.find_data path do |d,k,p|
           found     = true
-          last_data = d
-          break if d == match
+          last_data = d[k]
+          break if d[k] == match
         end
 
         assert found,
@@ -46,13 +64,31 @@ class Kronk
 
 
       ##
+      # Assert that no data points found with the given path are equal
+      # to the given match.
+      # Supports all DataSet#find_data path types.
+
+      def assert_data_at_not_equal data, path, match, msg=nil
+        last_data = nil
+
+        data_set = Kronk::DataSet.new data
+        data_set.find_data path do |d,k,p|
+          last_data = d[k]
+          break if d[k] == match
+        end
+
+        assert_not_equal match, last_data, msg
+      end
+
+
+      ##
       # Makes request to both uris and asserts that the parsed data they
       # return is equal. Compares response body if data is unparsable.
       # Supports all options of Kronk.compare.
 
       def assert_equal_responses uri1, uri2, options={}
-        resp1 = Request.retrieve_data_string uri1, options
-        resp2 = Request.retrieve_data_string uri2, options
+        resp1 = Kronk.retrieve_data_string uri1, options
+        resp2 = Kronk.retrieve_data_string uri2, options
 
         assert_equal resp1, resp2
       end
---
+++ test/test_assertions.rb
@@ -0,0 +1,93 @@
+require 'test/test_helper'
+require 'lib/kronk/test/assertions'
+
+class TestAssertions < Test::Unit::TestCase
+  include Kronk::Test::Assertions
+
+  def setup
+    @array = [:a, :b, {:foo => "bar", :bar => [:a, :b, {:foo => "other"}]}, :c]
+    @hash  = {:foo => "bar", :bar => [:a, :b, {:foo => "other"}], :a => [1,2,3]}
+  end
+
+
+  def test_assert_data_at
+    assert_data_at @array, "2/bar/2"
+    assert_data_at @hash, "bar/2"
+  end
+
+
+  def test_assert_data_at_failure
+    self.expects(:assert).with(false, "OOPS")
+    assert_data_at @array, "foobar", "OOPS"
+  end
+
+
+  def test_assert_no_data_at
+    assert_no_data_at @array, "foobar"
+  end
+
+
+  def test_assert_no_data_at_failure
+    self.expects(:assert).with(false, "OOPS")
+    assert_no_data_at @array, "**/foo", "OOPS"
+  end
+
+
+  def test_assert_data_at_equal
+    assert_data_at_equal @array, "2/bar/2/foo", "other"
+    assert_data_at_equal @hash, "bar/2/foo", "other"
+  end
+
+
+  def test_assert_data_at_equal_failure
+    self.expects(:assert).with(false, "OOPS")
+    self.expects(:assert_equal).with("nodice", nil, "OOPS")
+    assert_data_at_equal @array, "foobar", "nodice", "OOPS"
+  end
+
+
+  def test_assert_data_at_not_equal
+    assert_data_at_not_equal @array, "foobar", "thing"
+  end
+
+
+  def test_assert_data_at_not_equal_failure
+    self.expects(:assert_not_equal).with("bar", "bar", "OOPS")
+    assert_data_at_not_equal @array, "**/foo", "bar", "OOPS"
+  end
+
+
+  def test_assert_equal_responses
+    io        = StringIO.new mock_resp("200_response.json")
+    mock_resp = Kronk::Response.read_new io
+
+    Kronk::Request.expects(:retrieve).times(2).
+      with("host.com", :foo => "bar").returns mock_resp
+
+    assert_equal_responses "host.com", "host.com", :foo => "bar"
+  end
+
+
+  def test_assert_equal_responses_failure
+    mock_resp1 = Kronk::Response.read_new \
+                  StringIO.new mock_resp("200_response.json")
+
+    mock_resp2 = Kronk::Response.read_new \
+                  StringIO.new mock_resp("301_response.txt")
+
+    Kronk::Request.expects(:retrieve).
+      with("host1.com", :foo => "bar").returns mock_resp1
+
+    Kronk::Request.expects(:retrieve).
+      with("host2.com", :foo => "bar").returns mock_resp2
+
+    left  = Kronk::Diff.ordered_data_string mock_resp1.selective_data
+    right = mock_resp2.body
+
+    assert_not_equal left, right
+
+    self.expects(:assert_equal).with left, right
+
+    assert_equal_responses "host1.com", "host2.com", :foo => "bar"
+  end
+end
