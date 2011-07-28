3f6a431771a9857e40027908937c6e3ad805238d
jcastagna <jeremie.castagna@gmail.com>
2011-07-28 09:56:15 -0700

    updated stream output docs

--- lib/kronk/player/stream_output.rb
+++ lib/kronk/player/stream_output.rb
@@ -5,7 +5,19 @@ class Kronk
   # in chunked form, each chunk being one response and the number
   # of octets being expressed in plain decimal form.
   #
-  #   "23\r\nthis is the first chunk\r\n"
+  #   out   = Player::StreamOutput.new
+  #
+  #   io1   = StringIO.new "this is the first chunk"
+  #   io2   = StringIO.new "this is the rest"
+  #
+  #   kronk = Kronk.new
+  #   kronk.retrieve io1
+  #   out.result kronk
+  #   #=> "23\r\nthis is the first chunk\r\n"
+  #
+  #   kronk.retrieve io2
+  #   out.result kronk
+  #   #=> "16\r\nthis is the rest\r\n"
   #
   # Note: This output class will not render errors.
 
@@ -23,6 +35,7 @@ class Kronk
       output = "#{output.length}\r\n#{output.length}\r\n"
       $stdout << output
       $stdout.flush
+      output
     end
   end
 end
