ec7b79605071b7ce5e87eecf6e63e8a80dcefd17
jcastagna <jeremie.castagna@gmail.com>
2011-10-06 17:52:54 -0700

    update redirect verbose loc

--- lib/kronk.rb
+++ lib/kronk.rb
@@ -330,7 +330,7 @@ class Kronk
 
     rdir = options[:follow_redirects]
     while resp.redirect? && (rdir == true || rdir.to_s.to_i > 0)
-      Cmd.verbose "Following redirect to #{resp['Location']}"
+      Cmd.verbose "Following redirect to #{resp.location}"
       resp = resp.follow_redirect options_for_uri(resp.location)
       rdir = rdir - 1 if Fixnum === rdir
     end
