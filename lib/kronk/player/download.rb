d7eabd08a1e2c6da7967771ea31232504fb8582d
jcastagna <jeremie.castagna@gmail.com>
2012-05-12 21:08:45 -0700

    doc update

--- lib/kronk/player/download.rb
+++ lib/kronk/player/download.rb
@@ -1,23 +1,9 @@
 class Kronk
 
   ##
-  # Outputs Player results as a stream of Kronk outputs
-  # in chunked form, each chunk being one response and the number
-  # of octets being expressed in hexadecimal form.
+  # Outputs Player results as a list of files.
   #
-  #   out   = Player::StreamOutput.new
-  #
-  #   io1   = StringIO.new "this is the first chunk"
-  #   io2   = StringIO.new "this is the rest"
-  #
-  #   kronk = Kronk.new
-  #   kronk.request io1
-  #   out.result kronk
-  #   #=> "17\r\nthis is the first chunk\r\n"
-  #
-  #   kronk.request io2
-  #   out.result kronk
-  #   #=> "10\r\nthis is the rest\r\n"
+  #   player = Player::Download.new :dir => "foo/bar"
   #
   # Note: This output class will not render errors.
 
