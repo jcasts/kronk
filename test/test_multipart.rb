1158d3d74fa9e8729f07372b3ad97978c041b5c7
jcastagna <jeremie.castagna@gmail.com>
2012-05-21 22:59:56 -0700

    multipart tests

--- test/test_multipart.rb
+++ test/test_multipart.rb
@@ -63,6 +63,63 @@ class TestMultipart < Test::Unit::TestCase
 
 
   def test_to_io
-    
+    @multi.add "key1", "bar"
+    @multi.add "key2", "some value"
+    @multi.add "key3", "other thing"
+
+    io = @multi.to_io
+    assert_equal Kronk::MultipartIO, io.class
+    assert_equal 1, io.parts.length
+    assert_equal StringIO, io.parts.first.class
+
+    expected = <<-STR
+--foobar\r
+content-disposition: form-data; name=key1\r
+\r
+bar\r
+--foobar\r
+content-disposition: form-data; name=key2\r
+\r
+some value\r
+--foobar\r
+content-disposition: form-data; name=key3\r
+\r
+other thing\r
+--foobar--
+STR
+    assert_equal expected.strip, io.read
+  end
+
+
+  def test_to_io_with_file
+    file = File.open("test/mocks/200_response.json", "rb")
+    @multi.add "key1", "bar"
+    @multi.add "key2", "some value"
+    @multi.add "my_file", file
+
+    io = @multi.to_io
+    assert_equal 3, io.parts.length
+    assert_equal StringIO, io.parts.first.class
+    assert_equal file, io.parts[1]
+    assert_equal StringIO, io.parts.last.class
+
+    expected = <<-STR
+--foobar\r
+content-disposition: form-data; name=key1\r
+\r
+bar\r
+--foobar\r
+content-disposition: form-data; name=key2\r
+\r
+some value\r
+--foobar\r
+content-disposition: form-data; name=my_file; filename=200_response.json\r
+Content-Type: application/json\r
+Content-Transfer-Encoding: binary\r
+\r
+#{ File.read("test/mocks/200_response.json") }\r
+--foobar--
+STR
+    assert_equal expected.strip, io.read
   end
 end
